59fdb889ee98242b18c3ce248e8b3f7c39cc8cfd
Ankur Maurya <31955983+ankurjbs@users.noreply.github.com>
2024-05-18 12:27:12 +0530

    Revision with new Concept 1. Component instance works in Isolation 2. conditional content &  optimal way of updating State 3.Updating old state with new state 4. two way binding

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,17 +1,41 @@
-import { useState } from 'react';
-export default function Player({name,symbol}){
-    const [isEditing,setIsEditing] = useState(false);
-    function handlePlayer(){
-        setIsEditing(true);
-    }
-    return(
-        <li>
-            <span className="player">
-                {isEditing ? <span><input name="palyer" value={name} /> </span>: <span className="player-name">{name}</span>}
-              
-              <span className="player-symbol">{symbol}</span>
-            </span>
-            <button onClick={handlePlayer}>Edit</button>
-        </li> 
-    );
+import { useState } from "react";
+export default function Player({ initialName, symbol }) {
+  // we can use same state if we want to manage different piece of work
+  const [playerName,setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+  function handleEditClick() {
+    // we can set variable if ant types below
+
+    // {isEditing ? setIsEditing(false) : setIsEditing(true)};
+    //setIsEditing(isEditing ? false : true);
+    // but in boolean case above code is unnecessary in java script
+    //setIsEditing(!isEditing); // schedule a state update to true
+
+    //now In react, when updating your state base on the previous value of that state we cant use like above, we shoud pass a function to that state updating function. This is strong recommendation of the Reat team. This fn will automatically be called by React and will create the guaranteed latest state value (instatntly). if we intilize two times above fn. o/p should be true then false. but this not will work as expted (take 1/2 mili second). 
+
+    setIsEditing ((editing)=> !editing)
+  }
+
+  function handleChange(event){
+    //  onChange will triger for every keystroke, will provide an event object,contains the value entered by the user. So we use event object and this event provide argument for setPalyerName.This event have a 'target' property which will refer the element
+    setPlayerName(event.target.value);
+  }
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  let buttonCaption = "Edit";
+  if (isEditing) {
+    // this value prop does not change because this 'value' props sets the value that's shown in input box so we have to pass this through state
+    editablePlayerName = <input name="palyer" type="text" value={playerName} onChange={handleChange} />;
+    // we are displaying the name and ulpdate name using onChange. this is known as two-way binding
+    buttonCaption = "Save";
+  }
+  return (
+    <li>
+      <span className="player">
+        {editablePlayerName}
+
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{buttonCaption}</button>
+    </li>
+  );
 }
