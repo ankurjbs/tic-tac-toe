102d98ae8ba60fa5070beedbaf2d404ba2097785
ankurjbs <ankurjbs@gmail.com>
2024-05-21 17:31:42 +0530

    sharing state managment in Log componenet

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,30 @@ import Player from './components/Player';
 import Log from './components/Lgg';
 import { useState } from 'react';
 
+function deriveActivePlayer(gameTurns){
+  let currentPlayer = 'X';
+      console.log('rendering');
+      if(gameTurns.length>0 && gameTurns[0].player === 'X'){
+        currentPlayer = 'O';
+      }
+      return currentPlayer;
+}
 function App() { 
   // lift the state up to the closet ancestor (i.e app having two child gameboard and player) component that has access to all components that need to work with that state
   // ancestor passes the state value via props to the child component.
   // ancestor passes a fn that eventually changes the state via props to the child component. This allows the child component to initiate the state change.
 
   const [gameTurns,setGameTurns] = useState([]);
-  const [activePlayer,setActivePlayer] = useState('X');
+  // const [activePlayer,setActivePlayer] = useState('X');
+  const activePlayer = deriveActivePlayer(gameTurns);
 
   function handleSelectSquare(rowIndex,colIndex){
-    setActivePlayer((curAtivePlayer)=> curAtivePlayer === 'X'?'O':'X');
+    // setActivePlayer((curAtivePlayer)=> curAtivePlayer === 'X'?'O':'X');
     setGameTurns((prevTunrs)=>{
-      let currentPlayer = 'X';
-      console.log('rendering');
-      if(prevTunrs.length>0 && prevTunrs[0].player === 'X'){
-        console.log('change');
-        currentPlayer = 'O';
-      }
+      const currentPlayer = deriveActivePlayer(prevTunrs);
+
       const updatedTurns = [{square:{row:rowIndex,col:colIndex},player:currentPlayer},...prevTunrs];
+
       return updatedTurns;
     });
   }
@@ -34,7 +40,7 @@ function App() {
         </ol>
         <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns}/>
       </div>
-      Log
+      <Log turns={gameTurns} />
     </main>
   )
 }
