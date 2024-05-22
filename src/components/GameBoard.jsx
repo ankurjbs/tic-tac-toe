8c62624f80787c9c817149d5d02263cf55778270
ankurjbs <ankurjbs@gmail.com>
2024-05-22 11:41:40 +0530

    After setup the player symbol disable the button

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -33,7 +33,7 @@ export default function GameBoard({onSelectSquare,turns}) {
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
                 {/* <button onClick={()=>handleSelectSqaure(rowIndex,colIndex)}>{playerSymbol}</button> */}
-                <button onClick={()=>onSelectSquare(rowIndex,colIndex)}>{playerSymbol}</button>
+                <button onClick={()=>onSelectSquare(rowIndex,colIndex)} disabled = {playerSymbol !== null}>{playerSymbol}</button>
               </li>
             ))}
           </ol>
